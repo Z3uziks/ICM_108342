01f386e1de4948277ceed3ec28d74f474b4e4469
Z3uziks <henriquecc2012@hotmail.com>
2024-04-14 23:49:00 +0100

    Third/Last Exercise Activity#5 Done

---
+++ Lab1/Activity5_3/app/src/main/java/com/example/practice_composebasicsthird/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.practice_composebasicsthird
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practice_composebasicsthird.ui.theme.PracticeComposeBasicsThirdTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PracticeComposeBasicsThirdTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposableMain()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposableInfo(title: String,description: String,backgroundColor: Color,modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize().background(backgroundColor).padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun ComposableMain() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfo(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_desc),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfo(
+                title = stringResource(R.string.sec_title),
+                description = stringResource(R.string.sec_desc),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfo(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_desc),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfo(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_desc),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticeComposeBasicsThirdTheme {
+        ComposableMain()
+    }
+}
